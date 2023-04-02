f65e3c11841056aaab90beb0e7c70b5f7950961a
Sarah Jones <sarahrjones8@gmail.com>
2023-04-02 11:50:16 -0400

    HTML structural work and JS for mobile responsiveness (dropdown menu)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// function changeHeader() {
-//     let h1 = document.querySelector('h1')
-//     h1.textContent = 'no'
-// }
+//Makes the grocery list a drop down on mobile, insuring mobile responsiveness
+const listButton = document.getElementById("list-button")
+const tab = document.getElementById("tab")
+console.log(tab)
+listButton.addEventListener("click", function() {
+    console.log("button has been clicked")
 
-// changeHeader()
+    if (tab.style.height === "100px") {
+        tab.style.height = "500px";
+    } else {
+        tab.style.height = "100px";
+    }
+
+})
