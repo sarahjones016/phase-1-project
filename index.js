32a0f37b7a7540688cb59d4b2a728768c4c0d449
Sarah Jones <sarahrjones8@gmail.com>
2023-04-03 12:44:24 -0400

    html / js / css work pre-dbjson

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-//Makes the grocery list a drop down on mobile, insuring mobile responsiveness
+//Variables
 const listButton = document.getElementById("list-button")
 const tab = document.getElementById("tab")
-console.log(tab)
+const form = document.getElementById("form")
+const listForm = document.getElementById("listForm")
+
+
+//Makes the grocery list a drop down on mobile, insuring mobile responsiveness
 listButton.addEventListener("click", function() {
     console.log("button has been clicked")
 
-    if (tab.style.height === "100px") {
+    if (tab.style.height === "150px") {
         tab.style.height = "500px";
     } else {
-        tab.style.height = "100px";
+        tab.style.height = "150px";
     }
-
 })
+
+//Add To Grocery List Form
+listForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const newItem = document.createElement("li")
+    listForm.append(newItem)
+    newItem.textContent = e.target.item.value
+})
+
