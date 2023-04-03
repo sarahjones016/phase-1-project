ea639824a7e093708d30603ac909b65da073b0a2
Yinson Tso <yinson.tso@gmail.com>
2023-04-03 15:29:02 -0400

    working site

--- index.js
+++ index.js
@@ -6,52 +6,60 @@
 // changeHeader()
 
 //Variables
-const listButton = document.getElementById("list-button")
-const tab = document.getElementById("tab")
-const form = document.getElementById("form")
-const listForm = document.getElementById("listForm")
+const listButton = document.getElementById("list-button");
+const tab = document.getElementById("tab");
+const mealForm = document.getElementById("mealForm");
+const listForm = document.getElementById("listForm");
+const mealList = document.getElementById("mealList");
 
+fetch("http://localhost:3000/dishes")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => renderPage(data));
 
 //Makes the grocery list a drop down on mobile, insuring mobile responsiveness
-listButton.addEventListener("click", function() {
-    console.log("button has been clicked")
+listButton.addEventListener("click", function () {
+  console.log("button has been clicked");
 
-    if (tab.style.height === "150px") {
-        tab.style.height = "500px";
-    } else {
-        tab.style.height = "150px";
-    }
-})
+  if (tab.style.height === "150px") {
+    tab.style.height = "500px";
+  } else {
+    tab.style.height = "150px";
+  }
+});
 
 //Add To Grocery List Form
-listForm.addEventListener("submit", function(e) {
+listForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newItem = document.createElement("li");
+  listForm.append(newItem);
+  newItem.textContent = e.target.item.value;
+  listForm.reset();
+});
+
+// Render Page Info
+
+function renderPage(dishes) {
+  mealForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    const newItem = document.createElement("li")
-    listForm.append(newItem)
-    newItem.textContent = e.target.item.value
-})
+    resetForm();
 
-//Meal Selector Form
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    fetch("http://localhost:3000/dishes")
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => renderPage(data))
-})
+    dishes.forEach(function (dish) {
+      if (dish.type === e.target.cuisine.value) {
+        let dishName = document.createElement("h2");
+        dishName.textContent = dish.name;
+        let img = document.createElement("img");
+        img.src = dish.image;
 
-function renderPage(dishes){
-    // dishes.forEach(function(dish) {
-    //     console.log(dish)
-    // })
-
-    dishes.forEach((dish) => {
-        console.log(dish)
-
-    })
-
-    // for (const dish of dishes) {
-    //      console.logo(dish)
-    // }
+        mealList.append(dishName, img);
+      }
+    });
+    mealForm.reset();
+  });
 }
+
+function resetForm() {
+  mealList.innerHTML = " ";
+}
+//! On page render
